f8486d2e3d1db85f515819ee2eb8c6cc0b71a713
HiCitying <HiCitying@users.noreply.github.com>
2020-03-21 19:46:59 +0800

    1002 1003

--- Basic_Level_C++/1003/main.cpp
+++ Basic_Level_C++/1003/main.cpp
@@ -13,8 +13,8 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < n; ++i) {
-       cin >> s;
-       map<char, int> m;
+        cin >> s;
+        map<char, int> m;
 
         for (int j = 0; j < s.size(); ++j) {
             m[s[j]] ++;
