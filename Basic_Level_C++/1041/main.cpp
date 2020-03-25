067d4e4314882a51d5526f694bb5cf4035865ee0
HiCitying <HiCitying@users.noreply.github.com>
2020-03-25 13:57:16 +0800

    :tada:星期三/2020/3/25

--- Basic_Level_C++/1041/main.cpp
+++ Basic_Level_C++/1041/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <unordered_map>
 
+using namespace std;
+struct node{
+    string strn;
+    int cno;
+};
+
+// 1041 考试座位号 (15分)
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+    unordered_map<int, node> m;
+    int no;
+    node temp;
+    for (int i = 0; i < n; ++i) {
+        cin >> temp.strn >> no >> temp.cno;
+        m[no] = temp;
+    }
+    cin >> n;
+    for (int j = 0; j < n; ++j) {
+        cin >> no;
+        cout << m[no].strn << " " << m[no].cno << endl;
+    }
     return 0;
 }
